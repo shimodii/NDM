902f3cc03a3e968392148e5d8faa2dcec96def4d
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2021-09-24 03:31:50 +0330

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
-printf "[✓] hello, enter folder you want to save files: [like ~/Downloads/] \n"
+printf "[✓] hello, enter folder you want to save files: [like /home/amirmohammad/Downloads/] \n \n"
 read dlpath
 
 if [ -d $dlpath  ] 
 then
 		echo "[✓]Directory found."
 		cd $dlpath
-		printf "now enter URLs for download and part them with space [like -> URL1 URL2 ...]"
+		printf "now enter URLs for download and part them with space [like -> URL1 URL2 ...]: \n \n"
 		read URLS
-	    count=0 	
+	 	count=0 	
 		touch temp.sh
 		echo "#!/bin/bash" > temp.sh
 		for i in $URLS 
