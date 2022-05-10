e8c606380e14b0c32723836a1e2cf7e895bced7e
am-shm <am.shahmoradi93@gmail.com>
2022-05-10 13:56:43 +0430

    some changes for some test

--- dm_new.sh
+++ dm_new.sh
@@ -1,6 +1,6 @@
 #/bin/bash 
 
-current_date=$(date)
+current_date=$(date | awk '{print $2"_"$3"_"$6"_"$4}')
 download_dir="/tmp/$current_date"
 download_list_dir="/tmp/$current_date/download_list"
 download_path=""
